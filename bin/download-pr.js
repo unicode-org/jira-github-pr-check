39e7f1edf7cd288cc9d9505a3148c33141b6b540
Shane Carr <shane@unicode.org>
2018-10-18 17:22:33 -0700

    Adding download-pr convenience function.

---
+++ bin/download-pr.js
@@ -0,0 +1,37 @@
+#!/usr/bin/env node
+// © 2016 and later: Unicode, Inc. and others.
+// License & terms of use: http://www.unicode.org/copyright.html#License
+
+// Use this script to download a PR JSON for testing with check-single-pr.js.
+// Example: node bin/download-pr.js > inputs/lfstest0_4.json
+
+"use strict";
+
+require("dotenv").config();
+
+const github = require("../src/github-status");
+
+const argv = require("yargs")
+	.option("owner", {
+		default: "unicode-org"
+	})
+	.option("repo", {
+		default: "lfstest0"
+	})
+	.option("number", {
+		default: "4"
+	})
+	.argv;
+
+async function main() {
+	const pullRequest = await github.getPullRequest({
+		owner: argv.owner,
+		repo: argv.repo,
+		number: argv.number
+	});
+	return pullRequest;
+}
+
+main().then((pullRequest) => {
+	console.log(JSON.stringify(pullRequest));
+});
