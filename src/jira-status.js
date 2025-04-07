5fcbe623864c3c11f4ec4261ad27167e31d06d8a
Steven R. Loomis <srl295@gmail.com>
2025-04-07 16:26:10 -0500

    bug: bot got 403 but didn't propagate error For #70
    - improve error reporting on jira
    - add a 5 second retry

--- src/jira-status.js
+++ src/jira-status.js
@@ -18,15 +18,20 @@ if (process.env.JIRA_USERNAME && process.env.JIRA_PASSWORD) {
 	});
 }
 
-async function getStatus(issueKey) {
+/** actually get status */
+async function doGetStatus(issueKey) {
 	return new Promise((resolve, reject) => {
 		jira.issue.getIssue({
 			issueKey
-		}, (err, issue) => {
+		}, (err, issue, response) => {
 			if (err && err.errorMessages && /does not exist/.test(err.errorMessages[0])) {
 				resolve(null);
 			} else if (err) {
-				reject(err);
+				if (response && response.statusCode) {
+					reject(Error(`HTTP ${response.statusCode} ${response.statusMessage} on getIssue ${issueKey} - GET ${response.req.path}`, { cause: Error(response.body) }));
+				} else {
+					reject(err);
+				}
 			} else if (issue.fields) {
 				resolve(issue);
 			} else {
@@ -34,6 +39,24 @@ async function getStatus(issueKey) {
 			}
 		});
 	});
+}
+
+/** wrapper with some retry */
+async function getStatus(issueKey) {
+	try {
+		return await doGetStatus(issueKey); // if OK, return
+	} catch(e) {
+		console.error(`Error getting ${issueKey}: ${e.message} - will retry`);
+		// try to retry - once
+		const delay = (await import('delay')).default;
+		await delay(5000);
+		try {
+			return await doGetStatus(issueKey); // if OK, return
+		} catch(e2) {
+			console.error(e2);
+			throw e2;
+		}
+	}
 }
 
 function getUrl(issueKey) {
